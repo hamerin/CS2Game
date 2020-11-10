fa7992c23894388895d7fa4eb944bf800a08dd57
hamerin <hamerin04@gmail.com>
2020-11-10 18:38:58 +0900

    upd

--- src/constant.py
+++ src/constant.py
@@ -1,7 +1,7 @@
 from typing import Final, Tuple
 
-WIDTH: Final[int] = 2400
-HEIGHT: Final[int] = 1600
+WIDTH: Final[int] = 1200
+HEIGHT: Final[int] = 900
 WHITE: Final[Tuple[int, int, int]] = (255, 255, 255)
 BLACK: Final[Tuple[int, int, int]] = (0,   0,   0)
 BLUE: Final[Tuple[int, int, int]] = (0,   0, 255)
